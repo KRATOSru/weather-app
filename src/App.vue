54456bc693ae06658db3bbd746c0c2b04f0cc9f3
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2022-08-31 04:47:11 +0300

    SiteNavigation component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <h1>Hello App</h1>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <site-navigation />
+    <h1>Hello App!</h1>
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import {RouterView} from 'vue-router'
+import SiteNavigation from './components/SiteNavigation.vue'
 </script>
 
 <style lang="scss" scoped></style>
