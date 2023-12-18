1c7d9deb7f5414e11c9fa6152845703999fb70cc
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2023-12-18 12:15:55 +0300

    redirect route started

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+  <div
+    class="flex flex-col min-h-screen font-Roboto bg-weather-primary"
+  >
     <site-navigation />
     <RouterView />
   </div>
@@ -9,5 +11,3 @@
 import {RouterView} from 'vue-router'
 import SiteNavigation from './components/SiteNavigation.vue'
 </script>
-
-<style lang="scss" scoped></style>
